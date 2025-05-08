08bec0c6c3937616093dcf2713a0c75dd53a5538
unknown <pavanid2325@gmail.com>
2025-05-08 22:09:41 +0530

    t push commit commit commit git push

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import {connectDB} from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app  = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/products", (req,res) =>{});
+app.use(express.json());
 
-app.listen(5000, () =>{
+app.use("/api/products",productRoutes)
+
+app.listen(PORT, () =>{
     connectDB();
-    console.log("server started at http://localhost:5000");
+    console.log("server started at http://localhost:" + PORT);
 });
 
